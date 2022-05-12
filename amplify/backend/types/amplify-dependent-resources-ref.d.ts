f95f592a0b3d335ef0d828e0d8291eccd454b6a8
ClaytonDewey <clay@dryadmedia.com>
2022-05-11 21:02:26 -0500

    Ran amplify publish

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "fullstackapp8b1e88a8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
